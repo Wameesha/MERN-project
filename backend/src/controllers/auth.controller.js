14c9a3da8428a13fbf56be2b5d0f4eb1ff3d6510
Wameesha <rasanjani9jayasingha@gmail.com>
2025-06-23 15:47:45 +0530

    complete login and logout backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -33,19 +34,28 @@ export async function signup(req,res){
             profilepic: randomAvatar,
         })
 
-        // TODO: CREATE THE USER IN STREM AS WELL
+        try {
+            await upsertStreamUser({
+            id: newUser._id.toString(),
+            name: newUser.fullName,
+            image: newUser.profilepic || "",
+            });
+            console.log(`Stream user created for ${newUser.fullName}`);
+        } catch (error) {
+            console.error("Error creating Stream user:", error);
+        }
 
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, 
             { expiresIn: "7d" 
 
-         })
+         });
 
         res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
             httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
             sameSite: "strict", // Set to "lax" if you want to allow cross-site cookies
             secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-        })
+        });
 
         res.status(201).json({ success: true, user: newUser });
 
@@ -58,9 +68,45 @@ export async function signup(req,res){
 }
 
 export async function login(req,res){
-    res.send("Login Route");
+   try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and password are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, 
+            { expiresIn: "7d" 
+
+         });
+
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+            sameSite: "strict", // Set to "lax" if you want to allow cross-site cookies
+            secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+        });
+
+        res.status(200).json({ success: true, user });
+
+
+   } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Internal server error" });
+   }
 }
 
 export function logout(req,res){
-    res.send("Logout Route");
+    res.clearCookie("jwt")
+    res.status(200).json({success: true, message: "Logged out successfully" });
 }
