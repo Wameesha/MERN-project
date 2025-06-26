d593cbc8cc9c324b9b1c21d687e0f77712bdb31f
Wameesha <rasanjani9jayasingha@gmail.com>
2025-06-26 07:47:55 +0530

    frontend setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+const LoginPage = () => {
+    return <div className="login-page">
+        <h1>Login Page</h1>     
+        <p>Please enter your credentials to log in.</p>
+        </div>
+    };
+
+export default LoginPage;
