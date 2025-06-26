90aaf598e3556251895f8a23f31b978a186dd222
Wameesha <rasanjani9jayasingha@gmail.com>
2025-06-26 08:52:27 +0530

    tanstack query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,31 @@ import ChatPage from "./pages/ChatPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
+
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import {axiosInstance} from "./lib/axios.jsx";
+
+
 
 const App = () => {
+
+  //tanstack query
+  const {data, isLoading,error} = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  })
+
+  console.log(data);
+
+  
   return (
     <div className="500 h-screen" data-theme="night">
-   
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
